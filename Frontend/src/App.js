4f44ed32aacace4ff9b0626c60935a8b8bc01425
Wickeysl <101114673+Wickeysl@users.noreply.github.com>
2025-05-18 04:10:50 +0530

    Update App.js

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./Views/Home";
 import Community from "./Views/Community";
+
 function App() {
   return (
     <BrowserRouter>
