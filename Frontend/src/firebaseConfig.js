fad252fe643929ec698a4e5f382583aa05713fcf
Wickeysl <101114673+Wickeysl@users.noreply.github.com>
2025-05-18 05:07:38 +0530

    Update firebaseConfig.js

--- Frontend/src/firebaseConfig.js
+++ Frontend/src/firebaseConfig.js
@@ -7,7 +7,7 @@ const firebaseConfig = {
   authDomain: "pafmain-a0be6.firebaseapp.com",
   projectId: "pafmain-a0be6",
   storageBucket: "pafmain-a0be6.appspot.com",
-  messagingSenderId: "136832715572", 
+  messagingSenderId: "136832715572",
   appId: "1:136832715572:web:ac62d5fc2f9bb35cfff361"
 };
 // Initialize Firebase
