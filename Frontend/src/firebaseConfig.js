3429551a98f960c30be3e5c7cf6dd52f226f3246
Wickeysl <101114673+Wickeysl@users.noreply.github.com>
2025-05-18 04:21:25 +0530

    Update firebaseConfig.js

--- Frontend/src/firebaseConfig.js
+++ Frontend/src/firebaseConfig.js
@@ -10,7 +10,6 @@ const firebaseConfig = {
   messagingSenderId: "136832715572",
   appId: "1:136832715572:web:ac62d5fc2f9bb35cfff361"
 };
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
