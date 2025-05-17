60b6b2039942125d5626b295e181ea1d22feec9c
Wickeysl <101114673+Wickeysl@users.noreply.github.com>
2025-05-18 05:10:38 +0530

    Update firebaseConfig.js

--- Frontend/src/firebaseConfig.js
+++ Frontend/src/firebaseConfig.js
@@ -13,5 +13,5 @@ const firebaseConfig = {
 // Initialized Firebase
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
-export const auth = getAuth(app);
+export const auth = getAuth(app); 
 export const storage = getStorage(app);
