b2ecfda3b76537251dd9f9aec7ddec7eadedd780
Wickeysl <101114673+Wickeysl@users.noreply.github.com>
2025-05-18 05:06:12 +0530

    Update firebaseConfig.js

--- Frontend/src/firebaseConfig.js
+++ Frontend/src/firebaseConfig.js
@@ -5,7 +5,7 @@ import { getStorage } from "firebase/storage";
 const firebaseConfig = {
   apiKey: "AIzaSyCDP_N9fYjlJnshuc4jckOP5IMo0HSrbmo",
   authDomain: "pafmain-a0be6.firebaseapp.com",
-  projectId: "pafmain-a0be6", 
+  projectId: "pafmain-a0be6",
   storageBucket: "pafmain-a0be6.appspot.com",
   messagingSenderId: "136832715572",
   appId: "1:136832715572:web:ac62d5fc2f9bb35cfff361"
