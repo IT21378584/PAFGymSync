a108d1a2cec60d1da13b685d6cf5713c8ab66a48
Wickeysl <101114673+Wickeysl@users.noreply.github.com>
2025-05-18 05:03:51 +0530

    Update firebaseConfig.js

--- Frontend/src/firebaseConfig.js
+++ Frontend/src/firebaseConfig.js
@@ -1,7 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
-import { getStorage } from "firebase/storage";
+import { getStorage } from "firebase/storage"; 
 const firebaseConfig = {
   apiKey: "AIzaSyCDP_N9fYjlJnshuc4jckOP5IMo0HSrbmo",
   authDomain: "pafmain-a0be6.firebaseapp.com",
